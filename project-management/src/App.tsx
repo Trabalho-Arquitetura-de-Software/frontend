c7b8a5f7a100aa8604d1f80d65e7ac141ffebf27
Roberto Luis <130838869+RobertsLuis@users.noreply.github.com>
2025-04-13 22:44:36 -0300

    feature: adding-graphql-route-and-base-infos

--- project-management/src/App.tsx
+++ project-management/src/App.tsx
@@ -1,12 +1,31 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import AppRoutes from './Routes/routes'
+import { Toaster } from '@/components/ui/toaster'
+
+// Configurar o cliente Apollo com uma URL mockada para desenvolvimento
+const client = new ApolloClient({
+  uri: 'http://localhost:8080/graphql', // Substitua pela URL real quando disponível
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    mutate: {
+      errorPolicy: 'all'
+    },
+    query: {
+      errorPolicy: 'all'
+    }
+  }
+});
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<AppRoutes />} />
-      </Routes>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<AppRoutes />} />
+        </Routes>
+      </BrowserRouter>
+      <Toaster />
+    </ApolloProvider>
   )
 }
